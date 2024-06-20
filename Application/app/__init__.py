22727ea186cbf4c295527245ad4827028a2577b3
Huy Hoang Phung <phungh67@gmail.com>
2024-06-20 21:42:37 +0700

    feat: Implement login manager

--- Application/app/__init__.py
+++ Application/app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 from config import app_config
 
 db = SQLAlchemy()
+
+login_manager = LoginManager()
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
@@ -15,5 +18,11 @@ def create_app(config_name):
     @app.route('/')
     def hello_world():
         return "Hello, World!"
+    
+    # call the login manager extension
+
+    login_manager.init_app(app)
+    login_manager.login_message = "You must been logged in to view this content."
+    login_manager.login_view = "auth.login"
 
     return app
