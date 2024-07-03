8595cce171effd78dd40ac00783da8884cf28f9c
phungh67 <106119538+phungh67@users.noreply.github.com>
2024-07-03 10:56:46 +0700

    feat: Update variable file for Terraform

---
+++ Cloud/Terraform-code/variables.tf
@@ -0,0 +1,43 @@
+variables "region" {
+  description = "The region of your infrastructure"
+  type = string
+  default = "us-east-1"
+}
+
+variables "az-list" {
+  description = "The list of AZs associated with a specific region, used for HA purpose"
+  type = list(string)
+  default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+
+variables "name-prefix" {
+  description = "The name prefix for better resources management, format <p/s/d>:<region_code>"
+  type = string
+  default = "pue1"
+}
+
+### Networking variables
+
+variables "vpc-main-cidr" {
+  description = "Main CIDR for VPC"
+  type = string
+  default = "10.0.0.0/16"
+}
+
+variables "public-subnet-cidr" {
+  description = "CIDR for public subnets based on VPC's CIDR"
+  type = list(string)
+  default = ["10.0.10.0/24", "10.0.10.20.0/24", "10.0.30.0/24"]
+}
+
+variables "private-subnet-cidr" {
+  description = "CIDR for private subnets based on VPC's CIDR"
+  type = list(string)
+  default = ["10.0.11.0/24", "10.0.21.0/24", "10.0.31.0/24"]
+}
+
+variables "database-subnet-cidr" {
+  description = "CIDR for database resouces based on VPC's CIDR"
+  type = list(string)
+  default = ["10.0.12.0/24", "10.0.22.0/24", "10.0.32.0/24"]
+}
