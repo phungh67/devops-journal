614e8c8aa984011a16d504df1d19637d8e8dc566
phungh67 <phungh67@gmail.com>
2026-07-23 07:01:26 +0200

    feat: update verification with file path handling

--- ai-ops/utils/verifier.py
+++ ai-ops/utils/verifier.py
@@ -1,11 +1,14 @@
-import json
+import json, os
 from pathlib import Path
 
-def verify_template(file_path:str) -> dict:
+root_dir = Path("/home/huyhoang/devops-journal/ai-ops")
+
+def verify_template(file_name:str, module_name: str) -> dict:
     """Verification function, summary which was included in the template and what was missing
     
     Keyword arguments:
-    argument -- path leads to the file, using relative path in this case
+    file_name -- name of verification-needed file
+    module_name -- name of module that contains this file, mostly prompts module
     """
 
     assertion_dict = {
@@ -14,19 +17,31 @@ def verify_template(file_path:str) -> dict:
         "null_key": []
     }
 
-    with open(file_path, "r") as file:
-        template = json.load(file)
-    
-    for key in template.keys():
-        if not template.get(key):
-            assertion_dict["null_value"] += 1
-            assertion_dict["null_key"].append(key)
-        assertion_dict["total_keys"] += 1
+    file_path = root_dir / module_name / file_name
+
+    try:
+        with open(file_path, "r") as file:
+            template = json.load(file)
+            for key in template.keys():
+                if not template.get(key):
+                    assertion_dict["null_value"] += 1
+                    assertion_dict["null_key"].append(key)
+                assertion_dict["total_keys"] += 1
+
+    except FileNotFoundError:
+        print(f"Error: The file at {file_path} does not exist or missed spelling?")
     
     return assertion_dict
 
 if __name__ == "__main__":
     print("Test begin: ")
-    res = verify_template("test.json")
+    res = verify_template("log_analyzer.json", "prompts/log_analyzer")
 
     print(json.dumps(res, indent=4))
+
+    print("==================================================================\n")
+
+    if res.get("null_key") == 0:
+        print("RESULT: there are some null values in this file, verify it befor using...")
+    else:
+        print("RESULT: Passed")
